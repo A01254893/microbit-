026e42fc0e8f2cc2040571c6ea380b6b74b0c935
A01254893 <112582801+A01254893@users.noreply.github.com>
2022-09-21 12:41:24 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,14 @@
+basic.forever(function () {
+    basic.showNumber(input.temperature())
+    if (input.temperature() == 10) {
+        basic.showString("Vamos a esquiar")
+    } else if (input.temperature() == 20) {
+        basic.showString("Hágamos un picnic")
+    } else if (input.temperature() == 30) {
+        basic.showString("Vamos a la playa")
+    } else if (input.temperature() == 40) {
+        basic.showString("Mejor no salgas")
+    } else {
+    	
+    }
+})
